383765be6c6176eab9087a10139fddd57db599e1
Henrique Sousa Brito <henriquesousa5507@gmail.com>
2024-10-30 00:52:12 -0300

    Criando page de AddAnuncio

--- client/src/components/TopoCadAnuncio/TopoCadAnuncio.jsx
+++ client/src/components/TopoCadAnuncio/TopoCadAnuncio.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import "../../styles/TopoCadAnuncio/TopoCadAnuncio.css"
+import "../../styles/TopoCadAnuncio/TopoCadAnuncio.css";
 
-const TopoCadAnuncio = () => {
-  return(
+const TopoCadAnuncio = ({ rota }) => {
+  return (
     <div className="topo-container">
-      <div className="primeiro">
+      <div className={`primeiro ${rota === 1 ? "ativo" : ""}`}>
         <h2>1°</h2>
       </div>
-      <div className="segundo">
+      <hr/>
+      <div className={`segundo ${rota === 2 ? "ativo" : ""}`}>
         <h2>2°</h2>
       </div>
-      <div className="terceiro">
+      <hr/>
+      <div className={`terceiro ${rota === 3 ? "ativo" : ""}`}>
         <h2>3°</h2>
       </div>
     </div>
--- client/src/pages/CadAnuncio1.jsx
+++ client/src/pages/CadAnuncio1.jsx
@@ -2,12 +2,48 @@ import React from "react";
 import HeaderLogado from "../components/Header-logado/Header-logado";
 import Footer from "../components/Footer/Footer";
 import TopoCadAnuncio from "../components/TopoCadAnuncio/TopoCadAnuncio";
+import "../styles/CadAnuncioUser/CadAnuncioUser.css";
 
 const CadAnuncio = () =>{
   return(
     <div style={{display:"flex", flexDirection: "column"}}>
       <HeaderLogado></HeaderLogado>
-          <TopoCadAnuncio></TopoCadAnuncio>
+          <TopoCadAnuncio rota={1}></TopoCadAnuncio>
+          <div className="main-container">
+            <form action="" className="form-cad1">
+              <h2>Informações do imóvel</h2>
+              <input type="text" name="" id="" placeholder="Titulo do imóvel"/>
+              <select name="" id="">
+                <option value="">Casa</option>
+                <option value="">Apartamento</option>
+                <option value="">Kitnet</option>
+                <option value="">Sobrado</option>
+              </select>
+              <input className="sem-setas" type="number" name="" id="" placeholder="Aréa M² (ex: 40)"/>
+              <h3>Número de quartos</h3>
+              <div className="num-quartos">
+                <button className="button-Search">1</button>
+                <button className="button-Search">2</button>
+                <button className="button-Search">3</button>
+                <button className="button-Search">4+</button>
+              </div>
+              <h3>Quantidade de banheiros</h3>
+              <div className="num-quartos">
+                <button className="button-Search">1</button>
+                <button className="button-Search">2</button>
+                <button className="button-Search">3</button>
+                <button className="button-Search">4+</button>
+              </div>
+              <h3>Mobiliado</h3>
+              <div className="num-quartos">
+                <button className="button-Search">Sim</button>
+                <button className="button-Search">Não</button>
+              </div>
+              <input className="sem-setas" type="number" name="" id="" placeholder="Valor R$"/>
+              <br/>
+              <button className="btn-prox">Proximo</button>
+            </form>
+          </div>
       <Footer></Footer>
     </div>
   );
